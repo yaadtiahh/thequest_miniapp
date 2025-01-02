5cc1414e4e653a7cd59b36f020eeac2cda9a8d18
Lleanin <Fifashf@yandex.ru>
2025-01-02 22:41:10 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update, WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler
+
+# Загружаем переменные окружения
+load_dotenv()
+
+
+async def start(update: Update, context):
+    # Ваш актуальный ngrok URL
+    web_app_url = "/"
+
+    # Настраиваем кнопку с Mini App
+    button = KeyboardButton(
+        text="Открыть Mini App",
+        web_app=WebAppInfo(url=web_app_url)
+    )
+    reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+
+    # Отправляем сообщение с кнопкой
+    await update.message.reply_text("Привет! Вот твой Mini App:", reply_markup=reply_markup)
+
+# Инициализация приложения
+bot_token = os.getenv("TG_BOT_TOKEN")
+application = Application.builder().token(bot_token).build()
+application.add_handler(CommandHandler("start", start))
+
+# Запуск бота
+application.run_polling()
---
+++ thequest_tg/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThequestTgConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'thequest_tg'
